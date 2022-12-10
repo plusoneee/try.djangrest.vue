644b2c655d41db47e4668447c646e2c8e662d4f3
plusoneee <y02121148s@gmail.com>
2022-12-10 23:50:26 +0800

    add viewset version

--- MyAPI/backend/urls.py
+++ MyAPI/backend/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
-from .views import ArticleList, ArticleDetails
+from django.urls import path, include
+
+''' for APIView version '''
+# from .views import ArticleList, ArticleDetails
+
+from rest_framework.routers import DefaultRouter
+from .views import ArticleViewSet
+
+router = DefaultRouter()
+router.register('articles', ArticleViewSet, basename='articles')
 
 urlpatterns = [
-    path('articles/', ArticleList.as_view()),
-    path('articles/<slug:slug>/', ArticleDetails.as_view()),
+
+    path('', include(router.urls))
+    # for APIView version 
+    # path('articles/', ArticleList.as_view()),
+    # path('articles/<slug:slug>/', ArticleDetails.as_view()),
 ]
--- MyAPI/backend/views.py
+++ MyAPI/backend/views.py
@@ -1,10 +1,67 @@
 from .models import Article as ArticleModel
 from .serializers import ArticleSerializer
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework import viewsets
 
+class ArticleViewSet(viewsets.ViewSet):
+
+    def get_article(self, id):
+        try:
+            article = ArticleModel.objects.get(id=id)
+        except ArticleModel.DoesNotExist:
+            return None
+        return article
+
+    def list(self, request):
+        articles = ArticleModel.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        article = self.get_article(pk)
+    
+        if article is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+
+    def update(self, request, pk=None):
+        article = self.get_article(id=pk)
+        if article is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+      
+        serializer = ArticleSerializer(article, data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        article = self.get_article(pk)
+        if article:
+            article.delete()
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+''' for APIView version
+
+from rest_framework.views import APIView
 class ArticleList(APIView):
 
     def get(self, request):
@@ -60,3 +117,4 @@ class ArticleDetails(APIView):
             return Response(serializer.data)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+'''
